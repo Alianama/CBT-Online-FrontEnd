74717b604d28e645563fa56944a565bac536514e
ali purnama alam <alipurnama69@gmail.com>
2025-02-27 13:16:59 +0700

    fix(page) sidebar fix active & breadcrumb

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -2,13 +2,13 @@
 import {BadgeCheck, ChevronsUpDown, LogOut} from "lucide-react"
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
 import {
-   DropdownMenu,
-   DropdownMenuContent,
-   DropdownMenuGroup,
-   DropdownMenuItem,
-   DropdownMenuLabel,
-   DropdownMenuSeparator,
-   DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import {SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar} from "@/components/ui/sidebar"
 
