c97ffb5e513df9c0cb6dc2908a675c368d2ec29c
alipurnama <alipurnama69@gmail.com>
2025-03-15 20:44:43 +0700

    fix(auth) : if already login skip fetch api auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,27 +1,34 @@
-import {useNavigate, useParams} from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import {userAuth} from '@/app/api/api-cbt.ts';
-import {toast, Toaster} from "sonner";
-import {setAccessToken, setRefreshToken, setUserData} from "@/utils/storage.ts";
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { userAuth } from '@/app/api/api-cbt.ts';
+import { toast, Toaster } from "sonner";
+import { setAccessToken, setRefreshToken, setUserData } from "@/utils/storage.ts";
 import LoginLoadingAnimation from "@/components/ui/login-loading.tsx";
+import { isAuthenticated } from "@/utils/auth.ts";
 
 const LOGOUT_URL: string = import.meta.env.VITE_LOGOUT_URL;
+
 export default function Auth() {
-    const {token} = useParams();
+    const { token } = useParams();
     const navigate = useNavigate();
     const [message, setMessage] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
+
     useEffect(() => {
         (async () => {
+            if (await isAuthenticated()) {
+                navigate('/');
+                return;
+            }
             if (token) {
                 try {
                     setLoading(true);
                     const response = await userAuth(token);
                     if (response) {
-                        const {access_token, refresh_token, user_data} = response;
-                        setAccessToken(access_token)
-                        setRefreshToken(refresh_token)
-                        setUserData(user_data)
+                        const { access_token, refresh_token, user_data } = response;
+                        setAccessToken(access_token);
+                        setRefreshToken(refresh_token);
+                        setUserData(user_data);
                         console.log('✅ User authenticated:', response);
                         setMessage('Authentication successful! 🎉');
                         toast.success("Login success!");
@@ -38,24 +45,22 @@ export default function Auth() {
                     }
                 } catch (error) {
                     console.error('❌ Authentication error:', error);
-                    if (error) {
-                        setMessage(`Authentication failed! ${error ?? 'Unknown error'}`);
-                        toast.error('Login failed.');
-                    } else {
-                        setMessage('Authentication error! Please try again.');
-                        toast.error('Login error! Something went wrong.');
-                    }
+                    setMessage(`Authentication failed! ${error ?? 'Unknown error'}`);
+                    toast.error('Login failed.');
                     setTimeout(() => {
                         window.location.href = LOGOUT_URL;
                     }, 3000);
+                } finally {
+                    setLoading(false);
                 }
             }
         })();
     }, [token, navigate]);
+
     return (
-        <div>
-            <Toaster position="top-right" richColors/>
-            <LoginLoadingAnimation isLoading={loading} text={message}  />
-        </div>
+      <div>
+          <Toaster position="top-right" richColors />
+          <LoginLoadingAnimation isLoading={loading} text={message} />
+      </div>
     );
 }
